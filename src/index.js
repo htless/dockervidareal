24e1ba5f37b36bcefff8c0844f7bdd4d2178c278
Henrique Ricci <ricci.henrique@outlook.com>
2022-08-02 13:35:51 -0300

    add random background color

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ var express = require('express');
 var app = express();
 require('dotenv').config();
 
-const port = 3000;
+const port = 3001;
 const enviroment = process.env.ENVIROMENT;
 
+const randColor = () =>  {
+  return "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
+}
+
 app.get('/', function(req, res) {
-  res.send(`Olá turma 21 - Teste no ambiente ${enviroment}`);
+  const color = parseInt((Math.random() * Math.pow(255, 3)).toString(), 16)
+  res.send(`<body style="background-color: ${randColor()}">Olá turma 21 - Teste no ambiente ${enviroment}</body>`);
 });
 
 app.listen(port, () => console.log(`Aplicação rodando na porta ${port}!`))
