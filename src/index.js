929687aeb6ed496be5490e70333e01a5d68e04ad
Murilo Vieira <murilo.santos@betrybe.com>
2022-08-02 13:12:01 -0300

    Testing with dynamic port

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ var app = express();
 require('dotenv').config();
 
 const enviroment = process.env.ENVIROMENT;
+const port = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
   res.send(`Olá turma 21 - Teste no ambiente ${enviroment}`);
 });
 
-app.listen(3000, () => console.log(`Aplicação rodando na porta 3000!`))
+app.listen(port, () => console.log(`Aplicação rodando na porta ${port}!`))
