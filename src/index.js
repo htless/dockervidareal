46c3813bc832df95cf71255b13576781df33955a
Murilo Vieira <murilo.santos@betrybe.com>
2022-08-02 13:23:01 -0300

    Utilizando porta estática

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 require('dotenv').config();
 
+const port = 3000;
 const enviroment = process.env.ENVIROMENT;
-const port = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
   res.send(`Olá turma 21 - Teste no ambiente ${enviroment}`);
