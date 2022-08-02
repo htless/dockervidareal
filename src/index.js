970d289e42d4ab34a7a5d23e796dec5955adacba
Murilo Vieira <murilo.santos@betrybe.com>
2022-08-02 14:22:06 -0300

    Teste sem as cores

--- src/index.js
+++ src/index.js
@@ -5,13 +5,8 @@ require('dotenv').config();
 const port = process.env.PORT || 3000;
 const enviroment = process.env.ENVIROMENT;
 
-const randColor = () =>  {
-  return "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
-}
-
 app.get('/', function(req, res) {
-  const color = parseInt((Math.random() * Math.pow(255, 3)).toString(), 16)
-  res.send(`<body style="background-color: ${randColor()}">Olá turma 21 - Teste no ambiente ${enviroment}</body>`);
+  res.send(`Olá turma 21 - Teste no ambiente ${enviroment}`);
 });
 
 app.listen(port, () => console.log(`Aplicação rodando na porta ${port}!`))
