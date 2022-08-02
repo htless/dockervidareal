43147250600bbfedcc86816f83524a1974bf2ebb
Murilo Vieira <murilo.santos@betrybe.com>
2022-08-02 12:23:27 -0300

    Utilizando variáveis de ambiente

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
+require('dotenv').config();
 
-const port = 3000;
+const enviroment = process.env.ENVIROMENT;
 
 app.get('/', function(req, res) {
-  res.send('Olá turma 21 - Teste 5');
+  res.send(`Olá turma 21 - Teste no ambiente ${enviroment}`);
 });
 
-app.listen(port, () => console.log(`Aplicação rodando na porta ${port}!`))
+app.listen(3000, () => console.log(`Aplicação rodando na porta ${port}!`))
